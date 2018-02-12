dc6d210d99cf7999fa99d5f0d147b097e8970670
Elvin Hwang <hosung0519@hotmail.com>
2018-02-11 22:00:14 -0800

    Elvin Second Commit
    Updated Title Page and Such

---
+++ Assets/Scripts/LoadRule.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadRule : MonoBehaviour {
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Rule_Scene");
+    }
+}
